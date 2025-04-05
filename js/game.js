d289225830a4dcf4c7ca17a0e5d2624357df8abb
Luke Armenia <152218642+luke-la@users.noreply.github.com>
2025-04-05 03:01:25 -0700

    adds levels and refinements to player movement

--- js/game.js
+++ js/game.js
@@ -3,13 +3,16 @@ const ctx = canvas.getContext("2d");
 canvas.width = 948;
 canvas.height = 533;
 
-tileSize = Math.ceil(canvas.width / 24);
-frameRate = 60;
+const tileSize = Math.ceil(canvas.height / 12);
+const frameRate = 60;
+const gravity = 0.02;
+const termVelocity = 3
+const groundFriction = 0.95
 
 const player = {
   pos: {
-    x: 0, // in meters
-    y: 5, // +y moves down (same as screenspace)
+    x: 32, // in meters
+    y: 0, // +y moves down (same as screenspace)
     depth: 1.5, // affects which layer of map you interact with
   },
   size: {
@@ -19,7 +22,11 @@ const player = {
     screenHeight: tileSize,
   },
   speed: 2, // m/s
-  fallVel: 0,
+  velocity: {
+    x: 0,
+    y: 0,
+    d: 0
+  },
   onGround: false,
 };
 
@@ -28,34 +35,84 @@ draw();
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  let lightness = Math.max(0, 150 - player.pos.y)
-  ctx.fillStyle = `rgba(${lightness}, ${lightness}, 255, 1)`;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
   ctx.save();
   ctx.fillStyle = "black";
+  //ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.translate(
     canvas.width / 2 - player.size.screenWidth / 2,
     canvas.height / 2 - player.size.screenHeight / 2
   );
-  for (let floor of level1.floorData) {
-    const floorDimen = floor.getScreenDimensions(player.pos, tileSize);
+
+  //draw background
+  for (let bg of background.rigData) {
+    ctx.fillStyle = "lightGrey";
+    const rect = bg.getScreenDimensions(player.pos, tileSize)
     ctx.fillRect(
-      floorDimen.x,
-      floorDimen.y,
-      floorDimen.width,
-      floorDimen.height
+      rect.x,
+      rect.y,
+      rect.width,
+      rect.height
+    )
+  }
+
+  //draw level
+  const drawData = [...level1.floorData, ...level2.floorData]
+  drawData.sort(function(a, b) {a.depth - b.depth})
+  let playerDrawn = false
+
+  for (let floor of drawData) {
+    console.log("here")
+    if (floor.depth[1] < player.pos.depth && !playerDrawn) {
+      ctx.fillStyle = "red";
+      ctx.fillRect(
+        0,
+        (tileSize / 3) * -(player.pos.depth - 1),
+        player.size.screenWidth,
+        player.size.screenHeight
+      );
+      
+      playerDrawn = true
+    }
+
+    const topRect = floor.getScreenDimensionsTop(player.pos, tileSize);
+    ctx.fillStyle = "black";
+    ctx.fillRect(
+      topRect.x,
+      topRect.y,
+      topRect.width,
+      topRect.height
+    );
+
+    const sideRect = floor.getScreenDimensionsSide(player.pos, tileSize)
+    ctx.fillStyle = "gray";
+    ctx.fillRect(
+      sideRect.x,
+      sideRect.y,
+      sideRect.width,
+      sideRect.height
     );
   }
-  ctx.fillStyle = "red";
-  ctx.fillRect(
-    0,
-    (tileSize / 3) * -(player.pos.depth - 1),
-    player.size.screenWidth,
-    player.size.screenHeight
-  );
+  if (!playerDrawn) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(
+      0,
+      (tileSize / 3) * -(player.pos.depth - 1),
+      player.size.screenWidth,
+      player.size.screenHeight
+    );
+    
+    playerDrawn = true
+  }
+  
+  const lightness = Math.max(0, 100 - player.pos.y)
+  ctx.fillStyle = `rgba(${lightness}, ${lightness}, 255, 0.5)`;
+  const ocean = background.ocean.getScreenDimensions(player.pos, tileSize)
+  ctx.fillRect(ocean.x, ocean.y, ocean.width, ocean.height);
+
   ctx.restore();
+  
 }
+
 
 const pressedKeys = {
   w: false,
@@ -83,44 +140,55 @@ function toggleKeys(e, state = true) {
 setInterval(update, 1000 / frameRate);
 
 function update() {
+  const nearestLevel = levels.find((level) => player.pos.y > level.startingY && player.pos.y < level.endingY)
+
   //handle side to side movement
-  if (pressedKeys.a) player.pos.x -= player.speed / frameRate;
-  if (pressedKeys.d) player.pos.x += player.speed / frameRate;
+  if (pressedKeys.a) player.velocity.x = -player.speed;
+  if (pressedKeys.d) player.velocity.x = player.speed;
+  if (!pressedKeys.a && !pressedKeys.d) player.velocity.x *= groundFriction
+  // handle depth
+  if (pressedKeys.w) player.velocity.d = player.speed;
+  if (pressedKeys.s) player.velocity.d = -player.speed;
+  if (!pressedKeys.w && !pressedKeys.s) player.velocity.d *= groundFriction
 
   // handle jumping and falling
   if (pressedKeys.space && player.onGround) {
-    player.fallVel = -3;
+    player.velocity.y = -3;
+    player.pos.y -= 0.1
     player.onGround = false;
   }
+  
   if (!player.onGround) {
-    player.pos.y += player.fallVel / frameRate;
-    player.fallVel =
-      player.fallVel < 3
-        ? (player.fallVel += Math.abs(player.fallVel / frameRate))
-        : 3;
-    if (Math.abs(player.fallVel) < 0.1) player.fallVel = 0.1;
+    if (player.velocity.y < -0.2 || player.velocity.y > 0.2) player.velocity.y += gravity
+    else player.velocity.y += 0.1
+    if (player.velocity.y > termVelocity) player.velocity.y = termVelocity
   }
 
   // test for falling collision
-  player.onGround = level1.floorData.some(function (floor) {
-    if (floor.depth !== Floor.DepthType.all && floor.depth != Math.floor(player.pos.depth)) {
-      return false;
-    }
-    if (floor.x > player.pos.x + player.size.width || floor.x + floor.width < player.pos.x) {
-      return false;
-    }
-    if (floor.y < player.pos.y + player.size.height && floor.y > player.pos.y) {
-      player.pos.y = floor.y - player.size.height;
-      player.fallVel = 0;
-      return true;
-    }
-    else return false;
-  })
+  if (nearestLevel) {
+    player.onGround = nearestLevel.floorData.some(function (floor) {
+      if (player.pos.depth > floor.depth[1] || player.pos.depth < floor.depth[0]) {
+        return false;
+      }
+      if (floor.x > player.pos.x + player.size.width || floor.x + floor.width < player.pos.x) {
+        return false;
+      }
+      if (floor.y <= player.pos.y + player.size.height && floor.y > player.pos.y) {
+        player.pos.y = floor.y - player.size.height;
+        player.velocity.y = 0;
+        return true;
+      }
+      else return false;
+    })
+  }
+  else {
+    player.onGround = false
+  }
 
-  // handle depth
-  if (pressedKeys.w) player.pos.depth += player.speed / frameRate;
-  
-  if (pressedKeys.s) player.pos.depth -= player.speed / frameRate;
+  // move player
+  player.pos.x += player.velocity.x / frameRate
+  if (!player.onGround) player.pos.y += player.velocity.y / frameRate
+  player.pos.depth += player.velocity.d / frameRate
 
   // redraw
   draw();
--- js/levels.js
+++ js/levels.js
@@ -1,36 +1,110 @@
 class Floor {
   static DepthType = {
-    front: 0,
-    middle: 1,
-    back: 2,
-    all: 3,
+    front: [0, 1],
+    middle: [1, 2],
+    back: [2, 3],
+    all: [0, 3],
   };
-  constructor(startX, width, y, depth) {
-    this.x = startX;
-    this.width = width;
+  constructor(x, y, width, height, depth) {
+    this.x = x;
     this.y = y;
+    this.width = width;
+    this.height = height
     this.depth = depth;
+  }
+  getScreenDimensionsTop(playerPos, tileSize) {
+    const x = (this.x - playerPos.x) * tileSize;
+    const width = this.width * tileSize;
+
+    let y = (this.y - playerPos.y - (2 / 3)) * tileSize
+    const height = tileSize * (this.depth[1] - this.depth[0]) / 3;
+
+    if (this.depth !== Floor.DepthType.all) {
+      y += height * Math.abs(2 - this.depth[0]);
+    }
+
+    return { x, y, width, height };
+  }
+  getScreenDimensionsSide(playerPos, tileSize) {
+    const x = (this.x - playerPos.x) * tileSize;
+    const width = this.width * tileSize;
+
+    let y = (this.y - playerPos.y - (2 / 3)) * tileSize
+    const height = this.height * tileSize
+    const depth = tileSize * (this.depth[1] - this.depth[0]) / 3;
+
+    if (this.depth !== Floor.DepthType.all) {
+      y += depth * Math.abs(2 - this.depth[0]);
+    }
+
+    y += depth;
+
+    return { x, y, width, height };
+  }
+}
+
+class BackgroundDeco {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
   }
   getScreenDimensions(playerPos, tileSize) {
     const x = (this.x - playerPos.x) * tileSize;
     const width = this.width * tileSize;
 
     let y = (this.y - playerPos.y) * tileSize - (tileSize * 2 / 3);
-    let height = tileSize;
-
-    if (this.depth != Floor.DepthType.all) {
-      height /= 3
-      y += height * Math.abs(2 - this.depth);
-    }
+    let height = this.height * tileSize
 
     return { x, y, width, height };
   }
 }
 
+const background = {
+  ocean: new BackgroundDeco(-100, 0, 200, 500),
+  rigData: [
+    new BackgroundDeco(-25, -20, 5, 500),
+    new BackgroundDeco(20, -20, 5, 500),
+    new BackgroundDeco(-30, 6, 60, 5),
+    new BackgroundDeco(-35, 42, 70, 5),
+  ]
+}
+
 const level1 = {
-  startingY: 10,
+  startingY: 0,
   floorData: [
-    new Floor(-10, 15, 7, Floor.DepthType.all),
-    new Floor(5, 4, 7, Floor.DepthType.middle),
+    new Floor(18, 7, 15, 1, Floor.DepthType.all),
+    new Floor(12, 6, 4, 10, Floor.DepthType.all),
+    new Floor(-10, 10, 18, 1, Floor.DepthType.all),
+    new Floor(-13, 10, 3, 0.5, Floor.DepthType.back),
+    new Floor(-13, 10, 1, 0.5, Floor.DepthType.middle),
+    new Floor(-17, 10, 5, 0.5, Floor.DepthType.front),
+    new Floor(-19, 10, 2, 10, Floor.DepthType.all),
+    new Floor(-27, 7, 4, 10, Floor.DepthType.all),
   ],
+  endingY: 18,
 };
+
+const level2 = {
+  startingY: 25,
+  floorData: [
+    new Floor(-36, 42, 8, 1, Floor.DepthType.all),
+    new Floor(-24, 46, 4, 1, Floor.DepthType.all),
+    new Floor(-20, 43, 2, 1, Floor.DepthType.back),
+    new Floor(-20, 43, 2, 1, Floor.DepthType.middle),
+    new Floor(-18, 43, 4, 1, Floor.DepthType.back),
+    new Floor(-14, 43, 1, 1, Floor.DepthType.all),
+    new Floor(-12, 46, 11, 1, Floor.DepthType.all),
+    new Floor(-1, 46, 3, 1, Floor.DepthType.front),
+    new Floor(2, 46, 1, 1, Floor.DepthType.all),
+    new Floor(3, 46, 3, 1, Floor.DepthType.back),
+    new Floor(6, 46, 1, 1, Floor.DepthType.all),
+    new Floor(7, 46, 3, 1, Floor.DepthType.front),
+    new Floor(10, 46, 3, 1, Floor.DepthType.all),
+    new Floor(15, 44, 7, 1, Floor.DepthType.all),
+  ],
+  endingY: 50,
+};
+
+const levels = [level1, level2]
